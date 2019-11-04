810b6f57130b0fef1f14e88edcea5e1c7427d6e7
wozhendeaa <billyyang520@gmail.com>
2019-11-04 11:31:12 -0500

    metadata skeleton

--- part1.py
+++ part1.py
@@ -2,19 +2,115 @@
 # coding: utf-8
 
 import sys
+from _ctypes import Array
+
 import pyspark
 import string
 
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
+from pyspark import SparkContext
+from csv import reader
+from pyspark.sql import types
 
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, FloatType
+from pyspark.sql.types import *
 from pyspark.sql.window import Window
 from pyspark.sql.functions import *
+import json
+
+
+def output(metadata,_sc, table_name ):
+    results = {
+        "dataset_name": table_name,
+        "columns":metadata,
+        "key_column_candidates": ["test1", "test2"]
+    }
+    print(results)
+
+def profile(data,_sc, sqlContext, table_name):
+    results = {}
+#    for i in range(0, len(data.columns)):
+    for i in range(0, 1):
+        colName = data.columns[i]
+        query = "select distinct %s from %s " %\
+                (colName, table_name)
+        temp = sqlContext.sql(query)
+        null_count = temp.filter(temp[0].isNull()).count()
+        non_empty = temp.count() - null_count
+        results = {
+            "column_name": colName,
+            "number_non_empty_cells": non_empty,
+            "number_empty_cells": null_count,
+            "number_distinct_values": temp.distinct().count(),
+            "frequent_values": "null",
+            "data_types": ["test1","test2"]
+        }
+
+    return results
+
+
+def extractMeta(_sc, sql):
+    metaSchema = StructType([StructField("UniqueKey", StringType(), True),  # LongType
+                             StructField("CreatedDate", StringType(), True),  # TimestampType
+                             StructField("ClosedDate", StringType(), True),  # TimestampType
+                             StructField("Agency", StringType(), True),
+                             StructField("AgencyName", StringType(), True),
+                             StructField("ComplaintType", StringType(), True),
+                             StructField("Descriptor", StringType(), True),
+                             StructField("LocationType", StringType(), True),
+                             StructField("IncidentZip", StringType(), True),
+                             StructField("IncidentAddress", StringType(), True),
+                             StructField("StreeName", StringType(), True),
+                             StructField("CrossStreet1", StringType(), True),
+                             StructField("CrossStreet2", StringType(), True),
+                             StructField("IntersectionStreet1", StringType(), True),
+                             StructField("IntersectionStreet2", StringType(), True),
+                             StructField("AddressType", StringType(), True),
+                             StructField("City", StringType(), True),
+                             StructField("Landmark", StringType(), True),
+                             StructField("FacilityType", StringType(), True),
+                             StructField("Status", StringType(), True),
+                             StructField("DueDate", StringType(), True),  # TimestampType
+                             StructField("ResolutionDescription", StringType(), True),
+                             StructField("ResolutionActionUpdatedDate", StringType(), True),  # TimestampType
+                             StructField("Community", StringType(), True),
+                             StructField("BBL", StringType(), True),
+                             StructField("Borough", StringType(), True),
+                             StructField("X", StringType(), True),  # IntegerType
+                             StructField("Y", StringType(), True),  # IntegerType
+                             StructField("OpenDataChannelType", StringType(), True),
+                             StructField("ParkFacilityName", StringType(), True),
+                             StructField("ParkBorough", StringType(), True),
+                             StructField("VehicleType", StringType(), True),
+                             StructField("TaxiCompanyBorough", StringType(), True),
+                             StructField("TaxiPickupLocation", StringType(), True),
+                             StructField("BridgeHighwayName", StringType(), True),
+                             StructField("BridgeHighwayDirection", StringType(), True),
+                             StructField("RoadRamp", StringType(), True),
+                             StructField("BridgeHighwaySegment", StringType(), True),
+                             StructField("Latitude", StringType(), True),  # DoubleType
+                             StructField("Longitude", StringType(), True),  # DoubleType
+                             StructField("Location", StringType(), True)
+                             ])
+
+    data = _sc.read.csv(path=sys.argv[1], schema=metaSchema, sep='\t', header=True, inferSchema=False)
+    data.withColumn("CreatedDate", data.CreatedDate.cast(TimestampType()))
+    data.withColumn("ClosedDate", data.ClosedDate.cast(TimestampType()))
+    data.withColumn("DueDate", data.DueDate.cast(TimestampType()))
+    data.withColumn("ResolutionActionUpdatedDate", data.ResolutionActionUpdatedDate.cast(TimestampType()))
+    data.withColumn("X", data.X.cast(IntegerType()))
+    data.withColumn("Y", data.Y.cast(IntegerType()))
+    data.withColumn("Latitude", data.Latitude.cast(DoubleType()))
+    data.withColumn("Longitude", data.Longitude.cast(DoubleType()))
+    table_name = "ThreeOneOne"
+    data.createOrReplaceTempView(table_name)
+    metadata = profile(data,_sc, sql, table_name)
+    output(metadata,_sc, table_name)
+
+
 
 if __name__ == "__main__":
-
     sc = SparkContext()
 
     spark = SparkSession \
@@ -24,9 +120,10 @@ if __name__ == "__main__":
         .getOrCreate()
 
     sqlContext = SQLContext(spark)
+    extractMeta(spark, sqlContext)
 
     # get command-line arguments
     inFile = sys.argv[1]
-        
-    #Enter your modules here
+
+    # Enter your modules here
     sc.stop()
